95f4d5d87013a6d19a0b5b749787228ff22fdca7
Roman Tsukanov <roman@tsukanov.me>
2018-04-28 14:52:26 -0700

    Implement basic functionality

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+)
+
+const (
+	textWidth = 40
 )
 
 var (
@@ -15,21 +21,72 @@ var (
 		" ││ ││ ╯",
 		" │╰─╯│  ",
 		" ╰───╯  ",
-		"        ",
 	}
-	clippyWidth = len(clippy[0])
+	clippyHeight = len(clippy)
+	clippyWidth  = len(clippy[0])
+
+	bubbleSide = "│"
+	// Adding 2 to account for side characters
+	bubbleTop    = "╭" + strings.Repeat("─", textWidth+2) + "╮"
+	bubbleBottom = "╰" + strings.Repeat("─", textWidth+2) + "╯"
 )
 
+func splitString(s string, n int) []string {
+	sub := ""
+	subs := []string{}
+
+	runes := bytes.Runes([]byte(s))
+	l := len(runes)
+	for i, r := range runes {
+		sub = sub + string(r)
+		if (i+1)%n == 0 {
+			subs = append(subs, sub)
+			sub = ""
+		} else if (i + 1) == l {
+			subs = append(subs, sub)
+		}
+	}
+	// Adding missing spaces for the last one
+	last := subs[len(subs)-1]
+	subs[len(subs)-1] = last + strings.Repeat(" ", n-len(last))
+	return subs
+}
+
 // TODO: Allow to provide a title
-func generateTextBox(text string, width int) (out string) {
-	const (
-		side   = "│"
-		top    = "╭─╮"
-		bottom = "╰─╯"
-	)
-	return
+func generateTextBox(text string, width int) (rows []string) {
+	rows = append(rows, bubbleTop)
+	for _, substr := range splitString(text, width) {
+		rows = append(rows, bubbleSide+" "+substr+" "+bubbleSide)
+	}
+	rows = append(rows, bubbleBottom)
+	return rows
+}
+
+func combineWithClippy(rows []string) (output []string) {
+	// Prepending an empty row to align with Clippy
+	rows = append([]string{""}, rows...)
+
+	// Outputting the textbox
+	for i, row := range rows {
+		if i > clippyHeight {
+			row = strings.Repeat(" ", clippyWidth) + row
+		} else {
+			row = clippy[i] + row
+		}
+		output = append(output, row)
+	}
+
+	// Outputting the rest of Clippy if the textbox is not high enough
+	for i := len(rows); i < clippyHeight; i++ {
+		output = append(output, clippy[i])
+	}
+
+	return output
 }
 
 func main() {
-	fmt.Println("WIP")
+	text := ""
+	for _, row := range combineWithClippy(generateTextBox(text, textWidth)) {
+		fmt.Println(row)
+	}
 }
